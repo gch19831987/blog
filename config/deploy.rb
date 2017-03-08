97009be7a5cbc9598885a9f2c17f1449cef7849e
zqz <71264737@qq.com>
2017-03-09 06:40:42 +0800

    Add capistrano file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for current version of Capistrano
 lock "3.7.2"
 
-set :application, "my_app_name"
+set :application, "blog"
 set :repo_url, "https://github.com/gch19831987/blog.git"
 
 # Default branch is :master
